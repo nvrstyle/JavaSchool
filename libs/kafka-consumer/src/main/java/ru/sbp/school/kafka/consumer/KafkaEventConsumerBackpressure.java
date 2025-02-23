393e1dc68fd2add8b90c7af9086d27793cd075af
nvrstyle <nvrstyle@gmail.com>
2025-02-23 23:25:29 +0300

    Task4 одно подтверждение на пачку событий

--- libs/kafka-consumer/src/main/java/ru/sbp/school/kafka/consumer/KafkaEventConsumerBackpressure.java
+++ libs/kafka-consumer/src/main/java/ru/sbp/school/kafka/consumer/KafkaEventConsumerBackpressure.java
@@ -2,17 +2,23 @@ package ru.sbp.school.kafka.consumer;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import ru.sbp.school.kafka.api.*;
+import ru.sbp.school.kafka.consumer.commit.EventCommiter;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class KafkaEventConsumerBackpressure<T extends Identifiable> implements EventConsumer<T> {
 
     private final KafkaEventConsumer<T> kafkaConsumer;
+    private final EventCommiter eventCommiter;
+    private final Long commitTimeout;
 
     public KafkaEventConsumerBackpressure(ExecutorService executorService, EventProducer<Ack> producer, Consumer<String, T> consumer, Properties properties) {
+        this.commitTimeout = Long.valueOf(properties.getProperty("backpressure.timeout.ms"));
         this.kafkaConsumer = new KafkaEventConsumer<>(executorService, consumer, properties);
-        kafkaConsumer.addHandler((event) -> producer.send(Ack.acknowledge(event)));
+        this.eventCommiter = new EventCommiter(Executors.newSingleThreadScheduledExecutor(), commitTimeout, (ack) -> producer.send(ack));
+        kafkaConsumer.addHandler(eventCommiter::waitCommit);
         executorService.execute(kafkaConsumer);
     }
 
---
+++ libs/kafka-consumer/src/main/java/ru/sbp/school/kafka/consumer/commit/EventCommiter.java
@@ -0,0 +1,40 @@
+package ru.sbp.school.kafka.consumer.commit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.sbp.school.kafka.api.Ack;
+import ru.sbp.school.kafka.api.Identifiable;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+/**
+ * Сервис подтверждения доставки события
+ */
+public class EventCommiter {
+
+
+    private static final Logger log = LoggerFactory.getLogger(EventCommiter.class);
+
+    private final Map<UUID, Identifiable> waitCommitMap = new ConcurrentHashMap<>();
+    private final Long commitTimeOut;
+
+    public EventCommiter(ScheduledExecutorService scheduledService, Long commitTimeOut, Consumer<Ack> backOffCallBack) {
+        this.commitTimeOut = commitTimeOut;
+        scheduledService.schedule(() -> commit(backOffCallBack), commitTimeOut, TimeUnit.MILLISECONDS);
+    }
+
+    public void waitCommit(Identifiable event) {
+        waitCommitMap.putIfAbsent(event.getUuid(), event);
+        log.info("Событие uuid = {} добавлено в очередь ожидания подтверждения доставки", event.getUuid());
+    }
+
+    private void commit(Consumer<Ack> backOffCallBack) {
+        Ack acknowledge = Ack.acknowledge(waitCommitMap.values().stream().toList());
+        backOffCallBack.accept(acknowledge);
+    }
+}
